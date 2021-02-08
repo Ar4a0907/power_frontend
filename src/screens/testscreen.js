63f1567a3533d8298be5fa54771b31d4f2fe466e
GoblinskijGnom <cvetkov02@inbox.ru>
2021-02-08 11:17:32 +0200

    The clear react-app init

---
+++ src/screens/testscreen.js
@@ -0,0 +1,15 @@
+
+
+function TestScreen() {
+    return (
+      <div >
+        <header >
+        </header>
+        <body>
+            components here!
+        </body>
+      </div>
+    );
+  }
+  
+  export default TestScreen;
