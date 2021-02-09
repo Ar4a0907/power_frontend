fc76fcead4a54177ff1894deb86a876a4a0093bc
Artjoms Skurjats <ar4a0907@gmail.com>
2021-02-09 20:16:19 +0200

    Added Continue, Delete, Bookmark, Hamburger buttons

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ButtonStyles from './Button_style.module.scss';
+
+
 const ButtonMade = (props) => {
-/*{children,Medium,Small,XSmall,White,Blue,Gray,Pen} */
- /*   let ButtonClasses = classNames([ButtonStyles.Button],{
-        [ButtonStyles.Medium]:Medium,[ButtonStyles.Small]:Small,[ButtonStyles.XSmall]:XSmall,
-        [ButtonStyles.White]:White,[ButtonStyles.Blue]:Blue,[ButtonStyles.Gray]:Gray,[ButtonStyles.Pen]:Pen
-    })
-*/
+
     let Class = Object.entries(props).map(([key,value]) =>{
         if(value===true){
             return key
@@ -19,13 +16,12 @@ const ButtonMade = (props) => {
         ButtonClass += (ButtonStyles[Class[index]] + ' ');
     }
 
-    console.log(props);
-
-    return(
+    return (
         <button className={ButtonClass}>
             {props.children}
         </button>
-    )
+    );
+
 }
 
 
--- src/screens/testscreen.js
+++ src/screens/testscreen.js
@@ -14,21 +14,25 @@ function TestScreen() {
                         <ButtonMade Medium Gray><div>Button M</div></ButtonMade>
                         <ButtonMade Medium Blue><div>Button M</div></ButtonMade>
                         <ButtonMade Medium White><div>Button M</div></ButtonMade>
+                        <ButtonMade Medium Continue><div>Continue</div></ButtonMade>
                     </div>
                     <div className="Button--Test-Row">
                         <ButtonMade Small Gray><div>Button S</div></ButtonMade>
                         <ButtonMade Small Blue><div>Button S</div></ButtonMade>
                         <ButtonMade Small White><div>Button S</div></ButtonMade>
+                        <ButtonMade Small Bookmark><div>Bookmark</div></ButtonMade>
                     </div>
                     <div className="Button--Test-Row">
                         <ButtonMade XSmall Gray><div>XSmall</div></ButtonMade>
                         <ButtonMade XSmall Blue><div>XSmall</div></ButtonMade>
                         <ButtonMade XSmall White><div>XSmall</div></ButtonMade>
+                        <ButtonMade Small Delete><div>Delete</div></ButtonMade>
                     </div>
                     <div className="Button--Test-Row">
                         <ButtonMade Pen Gray/>
                         <ButtonMade Pen Blue/>
                         <ButtonMade Pen White/>
+                        <ButtonMade Hamburger/>
                     </div>
                 </div>
             </body>
