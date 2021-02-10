55c68d882ab31b411cccc9f0645daf81218d96a1
Artjoms Skurjats <ar4a0907@gmail.com>
2021-02-10 15:06:16 +0200

    Ru language removed, change to right quotes, camelCase for classes, fuctions, etc.

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import buttonStyles from './buttonStyle.module.scss';
 
 
-const Button = ({children, ...classes}) => {
+const Button = (props) => {
 
-    const buttonClass = Object.entries(classes).map(([key , value]) =>
-        buttonStyles[key] !== undefined ? buttonStyles[key] : ``).join(' ')
+    const buttonClass = Object.entries(props).map(([key , value]) =>
+        buttonStyles[key] !== undefined ? buttonStyles[key] : '').join(' ') + ' ' + (props.className ? props.className : '')
+    console.log(buttonClass);
 
     return (
         <button className = {buttonClass}>
-            {children}
+            {props.children}
         </button>
     );
 
--- src/screens/testScreen.js
+++ src/screens/testScreen.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Button from '../components/button/button';
-import './testScreenStyle.scss';
-
+import testStyle from './testScreenStyle.module.scss';
 
 function TestScreen() {
     return (
@@ -9,30 +8,30 @@ function TestScreen() {
             <header >
             </header>
             <body>
-                <div className="Button--Test">
-                    <div className="Button--Test-Row">
-                        <Button Medium Gray><div>Button M</div></Button>
-                        <Button Medium Blue><div>Button M</div></Button>
-                        <Button Medium White><div>Button M</div></Button>
-                        <Button Medium Continue><div>Continue</div></Button>
+                <div className={testStyle.test}>
+                    <div className={testStyle.testRow}>
+                        <Button medium gray className={testStyle['sampleClass']}><div>Button M</div></Button>
+                        <Button medium blue><div>Button M</div></Button>
+                        <Button medium white><div>Button M</div></Button>
+                        <Button medium continue><div>Continue</div></Button>
                     </div>
-                    <div className="Button--Test-Row">
-                        <Button Small Gray><div>Button S</div></Button>
-                        <Button Small Blue><div>Button S</div></Button>
-                        <Button Small White><div>Button S</div></Button>
-                        <Button Small Bookmark><div>Bookmark</div></Button>
+                    <div className={testStyle.testRow}>
+                        <Button small gray><div>Button S</div></Button>
+                        <Button small blue><div>Button S</div></Button>
+                        <Button small white><div>Button S</div></Button>
+                        <Button small bookmark><div>Bookmark</div></Button>
                     </div>
-                    <div className="Button--Test-Row">
-                        <Button XSmall Gray><div>XSmall</div></Button>
-                        <Button XSmall Blue><div>XSmall</div></Button>
-                        <Button XSmall White><div>XSmall</div></Button>
-                        <Button Small Delete><div>Delete</div></Button>
+                    <div className={testStyle.testRow}>
+                        <Button xSmall gray><div>XSmall</div></Button>
+                        <Button xSmall blue><div>XSmall</div></Button>
+                        <Button xSmall white><div>XSmall</div></Button>
+                        <Button small delete><div>Delete</div></Button>
                     </div>
-                    <div className="Button--Test-Row">
-                        <Button Pen Gray/>
-                        <Button Pen Blue/>
-                        <Button Pen White/>
-                        <Button Hamburger/>
+                    <div className={testStyle.testRow}>
+                        <Button pen gray/>
+                        <Button pen blue/>
+                        <Button pen white/>
+                        <Button hamburger/>
                     </div>
                 </div>
             </body>
