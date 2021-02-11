68e4c277045ef1082418ee180057f18333adaa97
Artjoms Skurjats <ar4a0907@gmail.com>
2021-02-11 16:21:25 +0200

    Marge fix

--- src/screens/testscreen.js
+++ src/screens/testscreen.js
@@ -1,41 +1,51 @@
 import React from 'react';
-import Block from '../components/Blocks/block';
-import testStyle from './testStyle.module.scss';
+import Button from '../components/button/button';
+import testStyle from './testScreenStyle.module.scss';
 
 const handleClick = (event) => {
-    event.preventDefault()
-    alert('Click!')
+    event.preventDefault();
+    console.log('Click!');
+}
+
+const hamburger = (event) => {
+    event.preventDefault();
+    alert('Hamburger!');
 }
 
 const testScreen = () => {
     return (
-      <div >
-        <header >
-        </header>
-        <body>
-            <div className={testStyle.container} onClick={handleClick}>
-                <Block big>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias
-                        aliquam architecto delectus deleniti et excepturi facere facilis fugit ipsam
-                        nemo non officia pariatur perferendis praesentium quibusdam quo, quod ratione
-                        saepe sint soluta tempora, ullam veritatis, vero voluptate. Aperiam consequatur
-                        eligendi inventore minus nesciunt odit quaerat soluta ullam velit vero.
-                    </p>
-                </Block>
-                <Block medium>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo nulla, sequi.
-                        Accusantium ad commodi dicta dolore doloribus, enim fugit libero minima neque
-                        nulla porro sequi.
-                    </p>
-                </Block>
-                <Block small>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, temporibus.</p>
-                </Block>
-            </div>
-        </body>
-      </div>
+        <div>
+            <header >
+            </header>
+            <body>
+                <div className={testStyle.test}>
+                    <div className={testStyle.testRow}>
+                        <Button onClick={handleClick} medium gray className={testStyle['sampleClass']}><div>Button M</div></Button>
+                        <Button medium blue><div>Button M</div></Button>
+                        <Button medium white><div>Button M</div></Button>
+                        <Button medium continue><div>Continue</div></Button>
+                    </div>
+                    <div className={testStyle.testRow}>
+                        <Button small gray><div>Button S</div></Button>
+                        <Button small blue><div>Button S</div></Button>
+                        <Button small white><div>Button S</div></Button>
+                        <Button small bookmark><div>Bookmark</div></Button>
+                    </div>
+                    <div className={testStyle.testRow}>
+                        <Button xSmall gray><div>XSmall</div></Button>
+                        <Button xSmall blue><div>XSmall</div></Button>
+                        <Button xSmall white><div>XSmall</div></Button>
+                        <Button small delete><div>Delete</div></Button>
+                    </div>
+                    <div className={testStyle.testRow}>
+                        <Button pen gray/>
+                        <Button pen blue/>
+                        <Button pen white/>
+                        <Button hamburger onClick={hamburger}/>
+                    </div>
+                </div>
+            </body>
+        </div>
     );
   }
   
