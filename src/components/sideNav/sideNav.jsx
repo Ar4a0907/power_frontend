230c5fe75e9caa06dedcf09cfdada454fcabf0ec
Artjoms Skurjats <ar4a0907@gmail.com>
2021-02-18 11:11:07 +0200

    Container for sideNav added

---
+++ src/components/sideNav/sideNav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import sideNavStyle from './sideNavStyles.module.scss';
+
+const SideNav = (props) => {
+    return (
+        <div className={sideNavStyle.sideBar}>
+            aaaaa
+        </div>
+    )
+}
+
+export default SideNav
