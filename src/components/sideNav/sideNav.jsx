70aec6df98b4bb8ed579168373fc02352c0ac6fa
Artjoms Skurjats <ar4a0907@gmail.com>
2021-02-18 16:51:39 +0200

    Title and logo added

--- src/components/sideNav/sideNav.jsx
+++ src/components/sideNav/sideNav.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import sideNavStyle from './sideNavStyles.module.scss';
+import cashImage from './cashImage.svg';
+import cloudImage from './cloudImage.svg';
 
 const SideNav = (props) => {
     return (
         <div className={sideNavStyle.sideBar}>
-            aaaaa
+            <div className={sideNavStyle.sideNavTitle}>
+                <div className={sideNavStyle.titleImage}>
+                    <img src={cloudImage} alt="Cloud" className={sideNavStyle.cloud}/>
+                    <img src={cashImage} alt="Cash" className={sideNavStyle.cash}/>
+                </div>
+                <span className={sideNavStyle.title}>
+                    cloudcash
+                </span>
+            </div>
         </div>
     )
 }
