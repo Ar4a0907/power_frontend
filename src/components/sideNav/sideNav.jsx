6968ad72ebb3cc0ddea64ae70103cdb9a49a7730
Artjoms Skurjats <ar4a0907@gmail.com>
2021-02-19 13:54:41 +0200

    SideNav styles added

--- src/components/sideNav/sideNav.jsx
+++ src/components/sideNav/sideNav.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 import sideNavStyle from './sideNavStyles.module.scss';
 import cashImage from './cashImage.svg';
 import cloudImage from './cloudImage.svg';
+import cloudCashImage from './cloudCashImage.svg';
 
 const SideNav = (props) => {
+
+    const handleClick = (evt) => {
+        evt.preventDefault()
+        console.log('Button Clicked!')
+    }
+
     return (
         <div className={sideNavStyle.sideBar}>
             <div className={sideNavStyle.sideNavTitle}>
@@ -14,6 +21,16 @@ const SideNav = (props) => {
                 <span className={sideNavStyle.title}>
                     cloudcash
                 </span>
+            </div>
+            <div className={sideNavStyle.sideNavMenu}>
+
+            </div>
+            <div className={sideNavStyle.sideNavFooter}>
+                <img src={cloudCashImage} className={sideNavStyle.cloudCash} alt="Cloud Cash"/>
+                <p className={sideNavStyle.slogan}>
+                    Give your money awesome space in cloud
+                </p>
+                <button className={sideNavStyle.premiumUpgrade} onClick={handleClick}>Upgrade to premium</button>
             </div>
         </div>
     )
