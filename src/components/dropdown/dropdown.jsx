43951b69ce556fc9b2c01b235c0928122b2d3103
Artjoms Skurjats <ar4a0907@gmail.com>
2021-02-23 16:14:29 +0200

    Dropdown rework

--- src/components/dropdown/dropdown.jsx
+++ src/components/dropdown/dropdown.jsx
@@ -1,30 +1,45 @@
 import React, {useState} from 'react';
 import dropdownStyle from './dropdownStyles.module.scss';
-
+import { useHistory } from 'react-router-dom';
 
 const Dropdown = ({label, items, className, ...classes}) => {
+
+    const history = useHistory();
 
     const defaultChoose = 'Select this'
 
     const [value, setValue] = useState(defaultChoose)
 
-    const handleChange = (evt) => {
-        setValue(evt.target.value)
+    const [shown, setShown] = useState(false)
+
+    const handleChange = idx => {
+        setValue(items[idx].label)
+        if (items[idx].link) {
+            history.push(`${items[idx].link}`)
+        }
+        if (items[idx].onClick) {
+            items[idx].onClick()
+        }
+    }
+
+    const handleClick = () => {
+        setShown(!shown)
     }
 
     const dropdownClass = Object.entries(classes).map(([key, value]) =>
         dropdownStyle[key] !== undefined ? dropdownStyle[key] : '').join(' ') + ' ' + (className ? className : '')
 
     return (
-        <div>
-            <label>{label}</label>
-            <select defaultValue={defaultChoose} onChange={handleChange}>
-                <option hidden>{value}</option>
-                {
-                    items.map( (element, idx) =>
-                        <option value={element.label} key={idx}>{element.label}</option>
-                    )}
-            </select>
+        <div className={dropdownClass + ' ' + dropdownStyle.customSelect} onClick={handleClick}>
+            <div className={dropdownStyle.label}><span>{label}</span></div>
+            <div><span>{value}</span></div>
+            {shown ?
+                items.map( (element, idx) =>
+                    <div>
+                        <span key={idx} value={idx} onClick={() => handleChange(idx)}>{element.label}</span>
+                    </div>
+                ) : null
+            }
         </div>
     )
 }
