28a08aefb7f8704308a54995be1b2e92bb95984d
ilja Cvetkov <iljacvetkov301@gmail.com>
2021-05-03 19:00:15 +0300

    add Edit button in options, probably need to rework after table refactoring

--- src/screens/Users/Users.jsx
+++ src/screens/Users/Users.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux';
 import { FormattedMessage, useIntl } from 'react-intl';
+import { history } from '../../_library';
 import { SideNav, Text } from '../../components/';
 import { Table } from '../../components';
 import usersPageStyle from './UsersPageStyle.module.scss';
@@ -18,6 +19,10 @@ const Users = (props) => {
             <FormattedMessage id="pba.loggedIn.title"/>, {props.name}!
         </Text>
     </div>
+
+    const optionsItems = [
+        {label: 'Edit', onClick: value => history.push(`/users/${value.nativeEvent.path[3].children[1].innerHTML}`)}
+    ];
 
     const sideNav = <SideNav menuComponents={[
         {label: intl.formatMessage({id:'pba.loggedIn.overview'}), link: '/overview', iconType: 'overview'},
@@ -48,7 +53,8 @@ const Users = (props) => {
                 ]}
                 expand={[
                     {label: 'login attempts', name: 'loginAttempts'}
-                ]} />
+                ]} 
+                options={optionsItems} />
             }
             </>
         </div>
